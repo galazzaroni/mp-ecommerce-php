f4c5eead10461b1c75694f53e927daf3a1cddf3e
Gonzalo Lazzaroni <galazzaroni@gmail.com>
2020-06-11 18:16:09 -0300

    fix url

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php 
+    define("URL", "https://galazzaroni-mp-commerce-php.herokuapp.com/");
     define("ACCESS_TOKEN", "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
     define("PUBLIC_KEY", "APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626");
     define("INTEGRATOR_ID", "dev_24c65fb163bf11ea96500242ac130004");
